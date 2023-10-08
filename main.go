eeef64dfc8d0358c6376b8e2436ee3ea29aaefce
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-10-08 16:01:42 -0300

    change port listened

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/taco", handlers.List)
-	err := http.ListenAndServe(":9000", r)
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		fmt.Println("Erro ao subir o server:", err)
 		return
