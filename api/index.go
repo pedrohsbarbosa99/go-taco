c943c002e5592035f98c2df5300f8378107d1a73
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-10-08 16:16:28 -0300

    change config to deploy vercel

--- api/index.go
+++ api/index.go
@@ -1,23 +1,20 @@
-package main
+package handler
 
 import (
-	"example/go-taco/handlers"
 	"fmt"
 	"net/http"
-
-	"github.com/go-chi/chi"
 )
 
-func main() {
-	r := chi.NewRouter()
-	r.Get("/api/taco", handlers.List)
-	err := http.ListenAndServe(":3000", r)
-	if err != nil {
-		fmt.Println("Erro ao subir o server:", err)
-		return
-	}
+// func main() {
+// 	r := chi.NewRouter()
+// 	r.Get("/api/taco", handlers.List)
+// 	err := http.ListenAndServe(":3000", r)
+// 	if err != nil {
+// 		fmt.Println("Erro ao subir o server:", err)
+// 		return
+// 	}
 
-}
+// }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
